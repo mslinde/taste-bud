4e813896e3163186e856bddc25e8f59df86ac113
Sofia Stephenson <sgandsteph@gmail.com>
2019-11-26 12:39:25 +0100

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating 20 fake users..."
+
+20.times do
+  user = User.new(
+
+  full_name: Faker::Name
+  email_address: Faker::Internet.email
+  encrypted_password:, Faker::Internet.password
+  full_name: Faker::Name
+  current_country: Faker::Address.country
+  origin_country: Faker::Address.country
+  birthday: Faker::Date.birthday(min_age: 18, max_age: 90)
+  gender: Faker::Gender.type
+  profile_photo:
+)
+
+  User.save!
+end
+
+puts "Finished!"
