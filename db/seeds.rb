cd194a12d2324a25f86373bee4c3e440981a5f44
Mark Slinde <slinde.mark@gmail.com>
2019-11-26 14:32:25 +0100

    Cleanup pre-new merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 
-# require "faker"
+require "faker"
 
-# 20.times do
-#   Vibe.create(name: Faker::FunnyName.name,
-#   :icon 'https://source.unsplash.com/random')
-# end
+puts 'Creating 20 vibes'
 
-# puts 'Creating 20 vibes'
+20.times do
+  Vibe.create(name: Faker::FunnyName.name,
+  :icon => 'https://source.unsplash.com/random')
+end
+
 # 20.times do
 #   language = Language.new(
 #     language:    Faker::ProgrammingLanguage.name,
@@ -14,5 +15,5 @@
 #   )
 #   language.save!
 # end
-# puts 'Finished!'
+puts 'Finished!'
 
