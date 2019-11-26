91de4b4a25076580936816bada450930d08a8345
Mark Slinde <slinde.mark@gmail.com>
2019-11-26 15:49:38 +0100

    Updated controller action for map

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,13 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.geocoded
+
+    @markers = @events.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude
+      }
+    end
   end
 
   def new
