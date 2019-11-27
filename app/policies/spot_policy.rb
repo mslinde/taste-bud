6aea6deb0a2b8cccfbbde058ba5c91691b5cd2cc
Sofia Stephenson <sgandsteph@gmail.com>
2019-11-27 18:28:49 +0100

    Create spot policy and spots controller

---
+++ app/policies/spot_policy.rb
@@ -0,0 +1,23 @@
+class EventPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def new?
+    create?
+  end
+end
