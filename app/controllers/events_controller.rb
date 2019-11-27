ca08b3d6795a8c981a88f5255aeb591b7e6413be
Mark Slinde <slinde.mark@gmail.com>
2019-11-27 11:23:34 +0100

    ap has popup window for each event with title and address

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
         lat: event.latitude,
         lng: event.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { event: event })
-        # image_url: helpers.asset_url('../assets/images/random')
+        # image_url: helpers.asset_url('/assets/images/random.png')
       }
     end
   end
