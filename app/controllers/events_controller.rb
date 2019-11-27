9711a2682ec7472f8f2c09e40a521aa2d1b17ada
Lauren St Jean <lauren.stjean@gmail.com>
2019-11-27 14:26:03 +0100

    Updating new form to authorize a user to create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,13 @@ class EventsController < ApplicationController
     @events = policy_scope(Event).order(created_at: :asc)
     @events = Event.all
     @vibes = Vibe.all
+    # might need for vibe filter
+    # @search = Vibe.find(vibe_params).events
   end
 
   def new
     @event = Event.new
+    authorize @event
   end
 
   def create
@@ -25,4 +28,8 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(:title, :address, :start_time, :description, :capacity, :vibe_id)
   end
+  # might need for vibe filter
+  # def vibe_params
+  #   params.require(:event).permit(:vibe_id)
+  # end
 end
