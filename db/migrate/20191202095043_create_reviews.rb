e1c9a18d6beca2415723825ccd61bd6ac8d2aafb
Sofia Stephenson <sgandsteph@gmail.com>
2019-12-02 11:13:27 +0100

    fix conflict table from location to neighborhood

--- db/migrate/20191202095043_create_reviews.rb
+++ db/migrate/20191202095043_create_reviews.rb
@@ -3,11 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.text :comment
       t.integer :vibe
-<<<<<<< HEAD
       t.integer :neighborhood
-=======
-      t.integer :location
->>>>>>> master
       t.integer :venue
       t.boolean :attended
       t.references :user, foreign_key: true
