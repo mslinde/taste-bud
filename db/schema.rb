93510186413edb1481166e4d456d807b54364a0a
Mark Slinde <slinde.mark@gmail.com>
2019-12-02 10:57:45 +0100

    Changed users_id to user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_132827) do
+ActiveRecord::Schema.define(version: 2019_12_02_095113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,11 +55,11 @@ ActiveRecord::Schema.define(version: 2019_11_29_132827) do
 
   create_table "user_languages", force: :cascade do |t|
     t.bigint "language_id"
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["language_id"], name: "index_user_languages_on_language_id"
-    t.index ["users_id"], name: "index_user_languages_on_users_id"
+    t.index ["user_id"], name: "index_user_languages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -91,5 +91,5 @@ ActiveRecord::Schema.define(version: 2019_11_29_132827) do
   add_foreign_key "spots", "events"
   add_foreign_key "spots", "users"
   add_foreign_key "user_languages", "languages"
-  add_foreign_key "user_languages", "users", column: "users_id"
+  add_foreign_key "user_languages", "users"
 end
