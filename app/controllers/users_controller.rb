b14635ac9b29f38c9906c67a78ec2f4782b6b6bd
Lauren St Jean <lauren.stjean@gmail.com>
2019-12-02 17:27:43 +0100

    review notice button only displays if you have review open

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   def show
     @events = Event.where(user_id: current_user)
     @spot = Spot.find_by_id(params[:id])
-    @to_review = Review.where(user:current_user, attended: nil).first
+    @to_review = Review.where(user: current_user, attended: nil).first
     authorize @events
-    authorize @to_review
   end
 end
