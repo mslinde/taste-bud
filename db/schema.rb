791276764c4865d5c6d053c79398797a3e73ff48
Sofia Stephenson <sgandsteph@gmail.com>
2020-02-02 23:22:06 +0100

    Creates Add comments to events migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_155250) do
+ActiveRecord::Schema.define(version: 2020_02_02_221746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_155250) do
     t.float "latitude"
     t.float "longitude"
     t.string "photo"
+    t.string "comment"
     t.index ["user_id"], name: "index_events_on_user_id"
     t.index ["vibe_id"], name: "index_events_on_vibe_id"
   end
