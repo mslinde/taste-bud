10ece8d597282dd170f0b57759893d9fa0927244
Sofia Stephenson <sgandsteph@gmail.com>
2020-02-04 23:27:22 +0100

    Changes spot_id column type to bigint in comments table db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_222152) do
+ActiveRecord::Schema.define(version: 2020_02_04_222510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "spot_id"
+    t.bigint "spot_id"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
