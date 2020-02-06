b21145818446da95d1d110314e4bd339027ac184
Sofia Stephenson <sgandsteph@gmail.com>
2020-02-06 17:12:41 +0100

    Adds comment pundit policies/authorizations

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,21 @@
+class CommentPolicy < ApplicationPolicy
+  class Scope < PunditScope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def new?
+    create?
+  end
+
+  def show?
+    true
+  end
+
+end
+
