431f2b98410e8f0c0ac3bfbfda72ec486cb3a1f0
Sofia Stephenson <sgandsteph@gmail.com>
2020-02-13 21:47:50 +0100

    Removes commented out code from controller. Removes new method from comments controller and comments routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,16 @@
 class CommentsController < ApplicationController
-  before_action :set_spot, only: [:new, :create, :destroy]
+  before_action :set_spot, only: [:create, :destroy]
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
-  def new
-    set_spot
-    @event = spot.event
-    @comment = Comment.new
-    authorize @comment
-  end
-
   def create
-    set_spot
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.spot = @spot
+    authorize @comment
     if @comment.save
       respond_to do |format|
-        format.html { redirect_back(fallback_location: root_path) }
+        format.html { redirect_to event_path(@event) }
         format.js
       end
     else
@@ -26,13 +19,13 @@ class CommentsController < ApplicationController
         format.js
       end
     end
-    authorize @comment
+
   end
 
    def destroy
     set_comment
-    @comment.destroy
     authorize @comment
+    @comment.destroygit
    end
 
   private
@@ -42,7 +35,7 @@ class CommentsController < ApplicationController
   end
 
   def set_comment
-    @comment = Comment.find(params[:id])
+    @comment = @spot.comments.find(params[:id])
   end
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :spots, only: [:destroy] do
     resources :reviews, only: [:new, :create]
-    resources :comments, only: [:new, :create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
